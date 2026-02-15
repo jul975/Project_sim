3141072799c24de2caace1aeb0d3063a823ddc16
Jules Lowette <juleslowette@gmail.com>
2026-02-15 23:35:25 +0100

    pp2 solved clearing up now

--- tests/engine_build/engineP2.py
+++ tests/engine_build/engineP2.py
@@ -11,45 +11,45 @@ class Agent:
     def __init__(self, id, starting_position, engine_rng):
         self.id = id
         self.position = starting_position
-        self.rng = np.random.default_rng(engine_rng)  
-
-    def step(self):
-        ## (partly fixed need clearing up ) bug is here, choosing direction and moving are two separate rng calls, so the order of the calls matters.
-        ## fix is to make one call to rng.integers(1, 3) * rng.choice([-1, 1])
-        # Question, should the agent have its own rng? or do the rng calls need to be made on an engine level? if so how to ensure each agent gets a unique seed?
-
-        # -> should the engine make the rng call and then pass the value to the agent?
-        # -> what is the point of having an rng on the agent if the engine is going to make the call?
-        # -> should all rng calls be made on the engine level?
+         
 
 
-        self.direction = self.rng.choice([-1, 1])
-        self.position += (self.rng.integers(1, 3) * self.direction)
+    def step(self, direction):        
+        self.position += direction
 
 
 class Engine:
-    def __init__(self, seed, agent_count ,starting_positions):
+    def __init__(self, seed, agent_count, reverse_order=False):
         
         self.rng = np.random.default_rng(seed)
         self.tick = 0
-        self.state = starting_positions
-        self.agents = [Agent(i, starting_positions, self.rng) for i in range(agent_count)]
+        self.state = self.initialize_state(agent_count)
+        if reverse_order:
+            self.state = self.state[::-1]
+        
+
+    def initialize_state(self, agent_count):
+        ''' 
+        The engine should be deterministic, so the initial state should be determined by the seed.
+        The seed should be used (or rng, need to clear up the difference between the two) to create the initial state.
+        '''
+        return [Agent(i, self.rng.integers(1, 30), self.rng) for i in range(agent_count)]
+
+
+
+
 
     def step(self):
-        # next_state = self.compute_next_state(self.state)
-        self.random_integer = self.rng.integers(1, 101)
-
-        for agent in self.agents:
-            agent.step()
-
-        self.state += self.random_integer
+        for agent in self.state:
+            direction = self.rng.choice([-1, 1])
+            agent.step(direction)
         self.tick += 1
 
     def run(self, n_steps):
         for _ in range(n_steps):
             self.step()
 
-        return self.random_integer, self.state
+        return self.state
     
 
 
@@ -57,19 +57,50 @@ class Engine:
 if __name__ == "__main__":
     seed = 42
     agent_count = 5
-    starting_positions = 0
+    
 
-    eng1 = Engine(seed, agent_count, starting_positions)
+
+    eng1 = Engine(seed, agent_count)
     eng1.run(10)
 
-    eng2 = Engine(seed, agent_count, starting_positions)
+    eng2 = Engine(seed, agent_count)
     eng2.run(10)
 
 
+    eng3 = Engine(seed, agent_count, reverse_order=True)
+    eng3.run(10)
+
+
+
+
+    # test 1: Same Seed → Identical World
+
+    print("Testing Same Seed => Identical World...")
+    print("================================================================")
+    print("case 1 engine 1 and engine 2 should be the same with same seed")
+    print("-----------------------------------------------------------------")
+    print("\n")
     print("Final positions eng1:")
-    for agent in eng1.agents:
+    for agent in eng1.state:
         print(f"Agent {agent.id} position: {agent.position}")
     print("\n")
     print("Final positions eng2:")
-    for agent in eng2.agents:
-        print(f"Agent {agent.id} position: {agent.position}")
+    for agent in eng2.state:
+        print(f"Agent {agent.id} position: {agent.position}")   
+    
+
+
+
+    # Shuffle Agent Iteration Order
+    # Test 2: Order Matters
+    print("\n")
+
+    print("Shuffling Agent Iteration Order...")
+    print("================================================================")
+    print("case 2 engine 3 should be different from engine 1 and 2 because the agent iteration order is reversed")
+    print("-----------------------------------------------------------------")
+    print("\n")
+    print("Final positions eng3:")
+    for agent in eng3.state:
+        print(f"Agent {agent.id} position: {agent.position}")   
+    
