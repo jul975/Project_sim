581acb2abf10d3c82d73d8b584f0e1535b5480f2
Jules Lowette <juleslowette@gmail.com>
2026-02-21 22:46:25 +0100

    dynamic energy system V1 is working, stil not refined enough as of the lack of agentcy range in the broad sence, will add topological layer next in order to increase stress and find way to restrut testing bed. CAVE non-extinction is forced now, need to remove when basic fromula parameters are worked out more cleanly (notion workbench)

--- dev/engine_build/config.py
+++ dev/engine_build/config.py
@@ -9,13 +9,19 @@ class SimulationConfig:
     # space
     world_size: int = 30
 
+    # agent spawn ranges 
+    min_location: int = 0
+    max_location: int = 30
+    spawn_range: tuple[int, int] = (min_location, max_location)
+
     # energy dynamics
     move_cost: int = 1
     energy_init_min: int = 20
     energy_init_max: int = 40
+    energy_init_range: tuple[int, int] = (energy_init_min, energy_init_max)
 
     # reproduction
-    reproduction_probability: float = 0.01
+    reproduction_probability: float = 0.5
     reproduction_probability_change_condition: float = 0.02 # NOTE: for testing
     reproduction_cost: int = 0
     reproduction_threshold: int = 0
